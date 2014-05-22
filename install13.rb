891148c448fb1c6a8b4cc384d677b27b5e429104
Ross Huggett <ross.huggett@gmail.com>
2014-05-22 14:22:34 +0100

    Updated README

--- install13.rb
+++ install13.rb
@@ -2,6 +2,6 @@
 
 require "FileUtils"
 
-templates_directory = ENV['HOME'] + '/Library/Preferences/IntelliJIdea12/templates'
+templates_directory = ENV['HOME'] + '/Library/Preferences/IntelliJIdea13/templates'
 FileUtils.cp ['fest_assert.xml', 'junit.xml', 'testng.xml', 'mockito.xml', 'surround.xml'], templates_directory
 print "Templates installed into #{templates_directory} "
